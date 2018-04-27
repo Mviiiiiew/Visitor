b03226233ad28f6e709c8203b0897b3592b82cb2
MAN <mview@hotmail.com>
2018-04-27 14:14:40 +0700

    cIT1

--- app/src/main/java/com/cit/visitormanagementsystem/MainActivity.java
+++ app/src/main/java/com/cit/visitormanagementsystem/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.gc.materialdesign.views.ButtonRectangle;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -14,6 +16,8 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txt_date;
     Button btn_Logout;
+    ButtonRectangle btn_in;
+    ButtonRectangle btn_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +37,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initInstances() {
         DateFormat df = new SimpleDateFormat(" d MMM yyyy, HH:mm");
         String date = df.format(Calendar.getInstance().getTime());
-
         txt_date = (TextView)findViewById(R.id.txt_date);
         btn_Logout = (Button)findViewById(R.id.btn_Logout);
         btn_Logout.setOnClickListener(this);
+        btn_in = (ButtonRectangle)findViewById(R.id.btn_in);
+        btn_out = (ButtonRectangle)findViewById(R.id.btn_out);
 
+        btn_in.setRippleSpeed(25);
+        btn_out.setRippleSpeed(25);
         txt_date.setText(date);
+
+        btn_in.setOnClickListener(this);
 
 
 
@@ -50,6 +59,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(btn_Logout == view){
             Intent intent  = new Intent(getApplicationContext(),LoginActivity.class);
             startActivity(intent);
+        }else if(btn_in == view){
+            Intent intent  = new Intent(getApplicationContext(),CarLicenseActivity.class);
+            startActivity(intent);
+
         }
     }
 }
